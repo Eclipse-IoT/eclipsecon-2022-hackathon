49a5a89a5345b9c2a76da1a9541a4743e4a7f10e
Ulf Lilleengen <lulf@redhat.com>
2022-08-17 14:16:52 +0200

    Add provisioner for gateway

---
+++ provisioner/gateway.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import blemesh
+try:
+  from gi.repository import GLib
+except ImportError:
+  import glib as GLib
+from dbus.mainloop.glib import DBusGMainLoop
+import dbus
+import dbus.service
+import dbus.exceptions
+import sys
+import os
+
+def main():
+	blemesh.configure_logging("device")
+
+	DBusGMainLoop(set_as_default=True)
+	blemesh.bus = dbus.SystemBus()
+
+	blemesh.mesh_net = dbus.Interface(blemesh.bus.get_object(blemesh.MESH_SERVICE_NAME,
+						"/org/bluez/mesh"),
+						blemesh.MESH_NETWORK_IFACE)
+
+	blemesh.mesh_net.connect_to_signal('InterfacesRemoved', blemesh.interfaces_removed_cb)
+
+	blemesh.app = blemesh.Application(blemesh.bus)
+
+	# Provisioning agent
+	blemesh.app.set_agent(blemesh.Agent(blemesh.bus))
+
+	first_ele = blemesh.Element(blemesh.bus, 0x00, 0x0100)
+	first_ele.add_model(blemesh.Model(0x1001))
+	first_ele.add_model(blemesh.Model(0x100D))
+	first_ele.add_model(blemesh.Model(0x1102))
+
+	second_ele = blemesh.Element(blemesh.bus, 0x01, 0x010D)
+	second_ele.add_model(blemesh.Model(0x1000))
+
+	third_ele = blemesh.Element(blemesh.bus, 0x02, 0x010E)
+	third_ele.add_model(blemesh.Model(0x1000))
+
+	blemesh.app.add_element(first_ele)
+	blemesh.app.add_element(second_ele)
+	blemesh.app.add_element(third_ele)
+
+	blemesh.mainloop = GLib.MainLoop()
+
+	blemesh.join()
+	blemesh.mainloop.run()
+
+
+if __name__ == '__main__':
+	main()
