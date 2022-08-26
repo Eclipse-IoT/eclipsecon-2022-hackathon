d97dc6d19b55d3b6e8fcdb0827a895ab7ba0887a
Ulf Lilleengen <lulf@redhat.com>
2022-08-26 08:54:12 +0200

    Add run scripts

---
+++ gateway/run.sh
@@ -0,0 +1,1 @@
+RUST_LOG=info cargo run -- --token 977fe713cd784eb2 --drogue-application eclipsecon-hackathon --drogue-device gateway --drogue-password hey-rodney
---
+++ simulator/run.sh
@@ -0,0 +1,1 @@
+RUST_LOG=info cargo run -- --token 069e78a0973a0c57
