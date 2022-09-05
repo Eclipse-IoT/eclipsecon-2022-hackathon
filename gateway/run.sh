a56b163bf55ddceada1047fe60ecd97e117e1424
Ulf Lilleengen <lulf@redhat.com>
2022-09-05 11:39:45 +0200

    Add lockfile

--- gateway/run.sh
+++ gateway/run.sh
@@ -1,1 +1,1 @@
-while true; do RUST_LOG="eclipsecon_gateway=trace" cargo run -- --token 93915057ec20f72e --drogue-application eclipsecon-hackathon --drogue-device gateway --drogue-password hey-rodney; done
+while true; do RUST_LOG="eclipsecon_gateway=trace" cargo run -- --token 9c7bfb6892080d9e --drogue-application eclipsecon-hackathon --drogue-device gateway --drogue-password hey-rodney; done
