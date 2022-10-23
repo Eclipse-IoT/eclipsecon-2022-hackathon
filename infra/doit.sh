75c5207dea1eb5905c583f2c71e48f2640986839
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2022-10-23 18:16:17 +0200

    Optimize

--- infra/doit.sh
+++ infra/doit.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 NAME=$1
 
-pushd ../firmware
-./flashsd.sh
-cargo flash --release --chip nRF52833_xxAA
-popd
+while true;
+do
+	out=$(probe-run --list-probes | grep Error)
+	if [ "$out" == "" ]; then
+		echo "Probe ready, flashing"
+		pushd ../firmware
+		./flashsd.sh
+		cargo flash --release --chip nRF52833_xxAA
+		popd
 
-./preprovision.sh $NAME
+		./preprovision.sh $NAME
+		exit 0
+	else
+		echo "Probe not ready yet"
+	fi
+	sleep 1
+done
