c426a2f43411870cbadc73af06288d916d9f7b90
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2022-10-24 15:03:21 +0200

    Add more service dependencies

--- infra/gateway/run_gateway.sh
+++ infra/gateway/run_gateway.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 GW=$1
+sleep 5
 /opt/gateway/eclipsecon-gateway --drogue-device $GW --drogue-application eclipsecon-hackathon --token dd26596e54e78fa2 --ca-path /etc/ssl/certs/ca-certificates.crt
